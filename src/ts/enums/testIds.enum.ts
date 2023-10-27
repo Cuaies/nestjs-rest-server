9fe37e0d1d54797f42510da7b09cdb018b9e6622
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-10-27 13:54:07 +0300

    docs(enums): create `testIds`

---
+++ src/ts/enums/testIds.enum.ts
@@ -0,0 +1,13 @@
+/**
+ * Enum for test ids.
+ */
+export enum TestIds {
+  /**
+   * A valid id that exists in the database.
+   */
+  EXISTENT = 1,
+  /**
+   * An invalid id that does not exist in the database.
+   */
+  NON_EXISTENT = 99999,
+}
