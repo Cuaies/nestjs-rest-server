1f9c9bd24430df97bd50f450c66b04fe7e296253
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-10-27 13:14:19 +0300

    feat(modules/auth): create auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { LoggerMiddleware } from '../core/middlewares';
 import { PrismaModule } from '../shared/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
