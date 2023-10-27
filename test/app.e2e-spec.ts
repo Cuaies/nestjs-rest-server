6b6c563ff617d532ece36055bbb40c0991bfbad1
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-10-27 14:08:51 +0300

    test(e2e): add coverage for `/auth` route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,13 @@
 import { TestingModule, Test } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from '../src/app/app.module';
 import { GeneralExceptionsFilter } from '../src/core/filters';
 import { setBaseUrl } from 'pactum/src/exports/request';
 import { PrismaService } from '../src/shared/prisma/prisma.service';
+import { spec } from 'pactum';
+import { seedData } from '../prisma/seedData';
+import { LoginDTO, RegisterDTO } from '../src/app/modules/auth/dto';
 
 describe('App (e2e)', () => {
   const PORT = 3005;
@@ -38,7 +41,111 @@ describe('App (e2e)', () => {
     await app.close();
   });
 
-  test.todo('Auth');
+  describe('/auth', () => {
+    describe('/register', () => {
+      const DTO: RegisterDTO = {
+        email: 'test@test.com',
+        password: 'password',
+        name: 'test',
+      };
+
+      beforeEach(async () => {
+        await prismaService.cleanDb();
+      });
+
+      test('should throw if no body is provided', async () => {
+        return spec()
+          .post('/auth/register')
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should throw if no email is provided', async () => {
+        return spec()
+          .post('/auth/register')
+          .withBody({ ...DTO, email: undefined })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should throw if no password is provided', async () => {
+        return spec()
+          .post('/auth/register')
+          .withBody({ ...DTO, password: undefined })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should throw if email already exists', async () => {
+        await prismaService.user.create({ data: DTO });
+
+        return spec()
+          .post('/auth/register')
+          .withBody({ ...DTO })
+          .expectStatus(HttpStatus.CONFLICT);
+      });
+
+      test('should register if no name is provided', async () => {
+        return spec()
+          .post('/auth/register')
+          .withBody({ ...DTO, name: undefined })
+          .expectStatus(HttpStatus.CREATED);
+      });
+
+      test('should register if name is provided', async () => {
+        return spec()
+          .post('/auth/register')
+          .withBody({ ...DTO })
+          .expectStatus(HttpStatus.CREATED);
+      });
+    });
+
+    describe('/login', () => {
+      const DTO: LoginDTO = seedData.users[0];
+
+      beforeAll(async () => {
+        await prismaService.cleanDb();
+        await prismaService.seedDatabase(seedData);
+      });
+
+      test('should throw if no body is provided', async () => {
+        return spec().post('/auth/login').expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should throw if no email is provided', async () => {
+        return spec()
+          .post('/auth/login')
+          .withBody({ ...DTO, email: undefined })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should throw if no password is provided', async () => {
+        return spec()
+          .post('/auth/login')
+          .withBody({ ...DTO, password: undefined })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should throw if email does not exist', async () => {
+        return spec()
+          .post('/auth/login')
+          .withBody({ ...DTO, email: 'invalid@test.com' })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test("should throw if password doesn't match", async () => {
+        return spec()
+          .post('/auth/login')
+          .withBody({ ...DTO, password: 'invalid' })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      test('should login', () => {
+        return spec()
+          .post('/auth/login')
+          .withBody({ ...DTO })
+          .expectStatus(HttpStatus.OK)
+          .stores('access_token', 'access_token');
+      });
+    });
+  });
 
   test.todo('Bills');
 
