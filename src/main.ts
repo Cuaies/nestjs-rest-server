1b6dde762813494abb1071dd312b9aa1528b3d92
cuaies <61624423+Cuaies@users.noreply.github.com>
2023-10-27 10:34:41 +0300

    feat(core/filters): create error handler (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app/modules/app.module';
+import { GeneralExceptionsFilter } from './core/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new GeneralExceptionsFilter(httpAdapter));
+
   await app.listen(3001);
 }
 bootstrap();
