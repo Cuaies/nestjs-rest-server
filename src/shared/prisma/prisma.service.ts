3274c7f27a118e363e58a545fa137b0307fda83d
Cuaies <61624423+Cuaies@users.noreply.github.com>
2023-10-30 17:04:36 +0200

    test(e2e): fix problems due to seeding remove prisma service methods

--- src/shared/prisma/prisma.service.ts
+++ src/shared/prisma/prisma.service.ts
@@ -1,24 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { SeedData } from '../../../prisma/ts/types';
-import { seedDatabase } from '../../../prisma/seed';
 
 /**
  * Provides a singleton instance of PrismaClient.
  */
 @Injectable()
-export class PrismaService extends PrismaClient {
-  /**
-   * Seeds the database with the seed data.
-   */
-  seedDatabase(seedData: SeedData) {
-    return seedDatabase(this, seedData);
-  }
-
-  /**
-   * Cleans the database.
-   */
-  cleanDb() {
-    return this.user.deleteMany({});
-  }
-}
+export class PrismaService extends PrismaClient {}
